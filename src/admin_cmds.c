1192789329f42d619cede650c9c3c458e430b8fd
bugsounet <github@bugsounet.fr>
2021-08-20 14:23:56 +0200

    translate to EN

--- src/admin_cmds.c
+++ src/admin_cmds.c
@@ -1,10 +1,10 @@
 /* src/admin_cmds.c - Diverses commandes pour admins
- * Copyright (C) 2004 ircdreams.org
+ * Copyright (C) 2021 ircdreams.org - bugsounet.fr
  *
- * contact: bugs@ircdreams.org
- * site web: http://ircdreams.org
+ * contact: bugsounet@bugsounet.fr
+ * site web: http://www.bugsounet.fr
  *
- * Services pour serveur IRC. Supporté sur IrcDreams V.2
+ * Services pour serveur IRC. Supporté sur IrcDreams V3
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -36,52 +36,52 @@
 #include "timers.h"
 
 int inviteme(aNick *nick, aChan *chaninfo, int parc, char **parv)
-{ 	/* invite des admins sur le salon d'infos */
-    putserv("%s " TOKEN_INVITE " %s :%s", os.num, nick->nick, bot.pchan);
-    return 1;
+{ /* invite des admins sur le salon d'infos */
+  putserv("%s " TOKEN_INVITE " %s :%s", os.num, nick->nick, bot.pchan);
+  return 1;
 }
 
 void CleanUp(void)
 {
-	anUser *u, *ut;
-	anAccess *a, *at;
+  anUser *u, *ut;
+  anAccess *a, *at;
 
-	int i = 0;
+  int i = 0;
 
-	free(quit_msg);
-	free(pasdeperm);
-	free(network);
+  free(quit_msg);
+  free(pasdeperm);
+  free(network);
 
-	purge_nickandserv();
+  purge_nickandserv();
 
-	for(i = 0;i < USERHASHSIZE;++i) for(u = user_tab[i];u;u = ut)
-	{
-		ut = u->next;
-		for(a = u->accesshead;a;free(a), a = at)
-		{
-			at = a->next;
-			if(a->info) free(a->info);
-		}
-		free(u);
-	}
+  for(i = 0;i < USERHASHSIZE;++i) for(u = user_tab[i];u;u = ut)
+  {
+    ut = u->next;
+    for(a = u->accesshead;a;free(a), a = at)
+    {
+      at = a->next;
+      if(a->info) free(a->info);
+    }
+    free(u);
+  }
 
-	return;
+  return;
 }
 
 int die(aNick *nick, aChan *chaninfo, int parc, char **parv)
 {
-	char *r = parc ? parv2msg(parc, parv, 1, 300) : quit_msg;
+  char *r = parc ? parv2msg(parc, parv, 1, 300) : quit_msg;
 
-	running = 0;
+  running = 0;
 
-	putserv("%s " TOKEN_QUIT " :%s", os.num, r);
-	putserv("%s " TOKEN_SQUIT " %s 0 :%s", bot.servnum, bot.server, r);
+  putserv("%s " TOKEN_QUIT " :%s", os.num, r);
+  putserv("%s " TOKEN_SQUIT " %s 0 :%s", bot.servnum, bot.server, r);
 
-	db_write_users();
-        write_cmds();
-        write_trusted();
+  db_write_users();
+  write_cmds();
+  write_trusted();
 
-	return 1;
+  return 1;
 }
 
 /*
@@ -89,148 +89,144 @@ int die(aNick *nick, aChan *chaninfo, int parc, char **parv)
  */
 int restart_bot(aNick *nick, aChan *chaninfo, int parc, char **parv)
 {
-	FILE *fuptime;
-	char *r = parc ? parv2msg(parc, parv, 1, 300) : quit_msg;
+  FILE *fuptime;
+  char *r = parc ? parv2msg(parc, parv, 1, 300) : quit_msg;
 
-	if((fuptime = fopen("uptime.tmp", "w"))) /* permet de maintenir l'uptime (/me tricheur)*/
-	{
-		fprintf(fuptime, "%lu", bot.uptime);
-		fclose(fuptime);
-	}
+  if((fuptime = fopen("uptime.tmp", "w"))) /* permet de maintenir l'uptime (/me tricheur)*/
+  {
+    fprintf(fuptime, "%lu", bot.uptime);
+    fclose(fuptime);
+  }
 
-	running = 0;
-	ConfFlag |= CF_RESTART;
+  running = 0;
+  ConfFlag |= CF_RESTART;
 
-	putserv("%s " TOKEN_QUIT " :%s [\2Redémarrage\2]", os.num, r);
-	putserv("%s "TOKEN_SQUIT" %s 0 :%s [\2RESTART\2]", bot.servnum, bot.server, r);
+  putserv("%s " TOKEN_QUIT " :%s [\2Restarting\2]", os.num, r);
+  putserv("%s "TOKEN_SQUIT" %s 0 :%s [\2RESTART\2]", bot.servnum, bot.server, r);
 
-	db_write_users();
+  db_write_users();
 
-        write_cmds();
-        write_trusted();
+  write_cmds();
+  write_trusted();
 
-	return 1;
+  return 1;
 }
 
 int chcomname(aNick *nick, aChan *chaninfo, int parc, char **parv)
 {
-	aHashCmd *cmd;
-	const char *newcmd = parv[2], *lastcmd = parv[1];
+  aHashCmd *cmd;
+  const char *newcmd = parv[2], *lastcmd = parv[1];
 
-	if(strlen(newcmd) > CMDLEN)
-		return osntc(nick, "Un nom de commande ne doit pas dépasser 14 caractères.");
+  if(strlen(newcmd) > CMDLEN)
+    return osntc(nick, "Command name length is restricted to %d chars.", CMDLEN);
 
-	if(!(cmd = FindCommand(lastcmd)) || *cmd->name == '\1')
-		return osntc(nick, "La commande \2%s\2 n'existe pas.", lastcmd);
+  if(!(cmd = FindCommand(lastcmd)) || *cmd->name == '\1')
+    return osntc(nick, "\2%s\2 is not a valid command.", lastcmd);
 
-	if(FindCommand(newcmd)) return osntc(nick, "La commande %s existe déjà.", newcmd);
+  if(FindCommand(newcmd)) return osntc(nick, "%s command already exists.", newcmd);
 
-	HashCmd_switch(cmd, newcmd);
-	osntc(nick, "La commande\2 %s\2 s'appelle maintenant\2 %s\2.", lastcmd, cmd->name);
-	BuildCommandsTable(1);
-	write_cmds();
-	return 1;
+  HashCmd_switch(cmd, newcmd);
+  osntc(nick, "The command \2%s\2 is now called \2%s\2.", lastcmd, cmd->name);
+  BuildCommandsTable(1);
+  write_cmds();
+  return 1;
 }
 
 int chlevel(aNick *nick, aChan *chaninfo, int parc, char **parv)
 {
-	const char *cmd = parv[1];
-	int level;
-	aHashCmd *cmdp;
+  const char *cmd = parv[1];
+  int level;
+  aHashCmd *cmdp;
 
-	if(!(cmdp = FindCommand(cmd))) return osntc(nick, "La commande \2%s\2 n'existe pas.", cmd);
+  if(!(cmdp = FindCommand(cmd))) return osntc(nick, "\2%s\2 is not a valid command.", cmd);
 
-	if(!is_num(parv[2]) || (level = atoi(parv[2])) > MAXADMLVL || level < 0)
-		return osntc(nick, "Veuillez préciser un level valide.");
+  if(!is_num(parv[2]) || (level = atoi(parv[2])) > MAXADMLVL || level < 0)
+    return osntc(nick, "This level is not valid.");
 
-	if(level == cmdp->level)
-		return osntc(nick, "%s est déjà au niveau %d.", cmd, level);
+  if(level == cmdp->level)
+    return osntc(nick, "%s is already LEVEL %d.", cmd, level);
 
-	if(!NeedNoAuthCmd(cmdp) && !level) 
-                   return osntc(nick, "La commande %s ne peut être utilisée que par des administrateurs"
-			" identifiés sur les services. Le niveau minimal est donc de 1.", cmd); 
+  if(!NeedNoAuthCmd(cmdp) && !level)
+                   return osntc(nick, "%s command can only be used by administrators identified on the services. The minimum LEVEL is 1.", cmd); 
 
-	if(level > nick->user->level)
-		return osntc(nick, "Vous ne pouvez pas donner un niveau supérieur au votre.");
-	else
-	{
-		if(level > 1) cmdp->flag |= CMD_ADMIN;
-		else cmdp->flag &= ~CMD_ADMIN;
-	}
+  if(level > nick->user->level)
+    return osntc(nick, "Level must be lower than yours.");
+  else
+  {
+    if(level > 1) cmdp->flag |= CMD_ADMIN;
+    else cmdp->flag &= ~CMD_ADMIN;
+  }
 
-	cmdp->level = level;
+  cmdp->level = level;
 
-	osntc(nick, "Le niveau de la commande %s est maintenant\2 %d\2 (%s).",
-				cmdp->name, cmdp->level, AdmCmd(cmdp) ? "Admin" : "User");
-	write_cmds();
-	BuildCommandsTable(1);
-	return 1;
+  osntc(nick, "LEVEL of the %s command is now\2 %d\2 (%s).",
+        cmdp->name, cmdp->level, AdmCmd(cmdp) ? "Admin" : "User");
+  write_cmds();
+  BuildCommandsTable(1);
+  return 1;
 }
 
 int disable_cmd(aNick *nick, aChan *chaninfo, int parc, char **parv)
 {
-	const char *cmd = parv[1];
-	int disable = 0, i = 0, nb = 0;
-	aHashCmd *cmdp = FindCommand(cmd);
-	aHashCmd *listcmd;
+  const char *cmd = parv[1];
+  int disable = 0, i = 0, nb = 0;
+  aHashCmd *cmdp = FindCommand(cmd);
+  aHashCmd *listcmd;
 
-	if (!strcasecmp(cmd, "-LIST")) {
+  if (!strcasecmp(cmd, "-LIST")) {
+    putserv("%s " TOKEN_NOTICE " %s :Disabled command list:", os.num, nick->numeric);
 
-        	putserv("%s " TOKEN_NOTICE " %s :Liste des commandes désactivées:", os.num, nick->numeric);
+    for(;i < CMDHASHSIZE;++i) for(listcmd = cmd_hash[i];listcmd;listcmd = listcmd->next)
+    {
+      if(listcmd->flag & CMD_DISABLE)
+      {
+        osntc(nick,"          LEVEL %d - %s", listcmd->level, listcmd);
+        nb++;
+      }
+    }
+    if(!nb)
+    osntc(nick, "No command is disabled.");
+    else
+    osntc(nick, "End of list. %d command disabled.", nb);
+  }
+  else {
+    if(!cmdp) return osntc(nick, "\2%s\2 is not a valid command.", cmd);
 
-        	for(;i < CMDHASHSIZE;++i) for(listcmd = cmd_hash[i];listcmd;listcmd = listcmd->next)
-        	{
-                	if(listcmd->flag & CMD_DISABLE)
-                	{
-                        	osntc(nick,"          Level %d - %s", listcmd->level, listcmd);
-                        	nb++;
-                	}
-         	}
-        	if(!nb)
-        	osntc(nick, "Aucune commande désactivée.");
-        	else
-        	osntc(nick, "Fin de la liste. %d commande%s désactivée%s.",nb, nb>1 ? "s" : "", nb>1 ? "s" : "");
-	}
-	else {
-	
-		if(!cmdp) return osntc(nick, "La commande \2%s\2 n'existe pas.", cmd);
+    if(!parv[2]) return 0;
 
-		if(!parv[2]) return 0;
+    if(!strcasecmp(parv[2], "ON")) disable = 1;
+    else if(strcasecmp(parv[2], "OFF")) return osntc(nick, "Argument must be ON or OFF.");
+    if(!strcasecmp(cmdp->corename, "DISABLE")
+      || !strcasecmp(cmdp->corename, "CHCOMNAME")
+      || !strcasecmp(cmdp->corename, "CHLEVEL"))
+        return osntc(nick, "Please don't DISABLE \2%s\2.", cmd);
 
-		if(!strcasecmp(parv[2], "ON")) disable = 1;
-		else if(strcasecmp(parv[2], "OFF")) return osntc(nick, "La valeur doit être ON ou OFF");
-		if(!strcasecmp(cmdp->corename, "DISABLE")
-			|| !strcasecmp(cmdp->corename, "CHCOMNAME")
-			|| !strcasecmp(cmdp->corename, "CHLEVEL")
-			|| !strcasecmp(cmdp->corename, "LOGIN")) /* ça nuirait à tout.. */
-				return osntc(nick, "Veuillez ne pas desactiver \2%s\2.", cmd);
+    if(disable) cmdp->flag |= CMD_DISABLE;
+    else cmdp->flag &= ~CMD_DISABLE;
 
-		if(disable) cmdp->flag |= CMD_DISABLE;
-		else cmdp->flag &= ~CMD_DISABLE;
-
-		osntc(nick, "La commande %s est maintenant \2%sactivée\2", cmd, disable ? "des" : "");
-		BuildCommandsTable(1);
-		write_cmds();
-	}
-	return 1;
+    osntc(nick, "%s command is now \2%s\2", cmd, disable ? "disabled" : "enabled");
+    BuildCommandsTable(1);
+    write_cmds();
+  }
+  return 1;
 }
 
 int rehash_conf(aNick *nick, aChan *chan, int parc, char **parv)
 {
-	if(load_config(FICHIER_CONF) == -1)
-		return osntc(nick, "Une erreur s'est produite lors de l'actualisation...");
+  if(load_config(FICHIER_CONF) == -1)
+    return osntc(nick, "An error occurred while rehashing ...");
 
-	return osntc(nick, "Configuration actualisée avec succès");
+  return osntc(nick, "configuration updated successfully");
 }
 
 int showconfig(aNick *nick, aChan *c, int parc, char **parv)
 {
-	osntc(nick, "Etat des variables de configuration de PDreams %s", SPVERSION);
-	osntc(nick, "Nom du réseau : %s - Uplink: %s:%d", network, bot.ip, bot.port);
-	osntc(nick, "Nom du serveur des Services : %s (%s) [Num: %s]", bot.server, bot.name, bot.servnum);
-	osntc(nick, "Salon de controle de %s: %s", os.nick, bot.pchan);
-	osntc(nick, "Message de quit : %s", quit_msg);
-	osntc(nick, "Message PASDEPERM : %s", pasdeperm);
-	osntc(nick, "Scan Msg: %s", scanmsg);
-	return 1;
+  osntc(nick, "PDreams %s configuration", SPVERSION);
+  osntc(nick, "Network Name : %s - Uplink: %s:%d", network, bot.ip, bot.port);
+  osntc(nick, "Server Name: %s (%s) [Num: %s]", bot.server, bot.name, bot.servnum);
+  osntc(nick, "Control Channel of %s: %s", os.nick, bot.pchan);
+  osntc(nick, "Quit Msg: %s", quit_msg);
+  osntc(nick, "No permission Msg: %s", pasdeperm);
+  osntc(nick, "Scan Msg: %s", scanmsg);
+  return 1;
 }
--- src/admin_manage.c
+++ src/admin_manage.c
@@ -1,10 +1,10 @@
 /* src/admin_manage.c - commandes pr gerer les admins
- * Copyright (C) 2004-2005 ircdreams.org
+ * Copyright (C) 2021 ircdreams.org - bugsounet.fr
  *
- * contact: bugs@ircdreams.org
- * site web: http://ircdreams.org
+ * contact: bugsounet@bugsounet.fr
+ * site web: http://www.bugsounet.fr
  *
- * Services pour serveur IRC. Supporté sur IrcDreams V.2
+ * Services pour serveur IRC. Supporté sur IrcDreams V3
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -33,23 +33,23 @@ aNick **adminlist = NULL;
 int adminmax = 0; 
     
 /* Try to insert a new admin in list, otherwise append it. */ 
-int adm_active_add(aNick *nick) 
-{ 
-        int i = 0; 
- 
-        while(i < adminmax && adminlist[i]) ++i; /* look for a free slot */ 
-        /* can't find one, make the list grow up */ 
-        if(i >= adminmax) adminlist = realloc(adminlist, sizeof *adminlist * ++adminmax); 
-        adminlist[i] = nick; /* insert it into the free slot */ 
-        return adminmax; 
+int adm_active_add(aNick *nick)
+{
+  int i = 0;
+
+  while(i < adminmax && adminlist[i]) ++i; /* look for a free slot */
+  /* can't find one, make the list grow up */
+  if(i >= adminmax) adminlist = realloc(adminlist, sizeof *adminlist * ++adminmax);
+  adminlist[i] = nick; /* insert it into the free slot */
+  return adminmax;
 } 
     
-/* Try to find admin in list and empties the slot */ 
-int adm_active_del(aNick *nick) 
-{ 
-        int i = 0; 
-    
-        while(i < adminmax && adminlist[i] != nick) ++i; /* searching in list.. */ 
-        if(i < adminmax) adminlist[i] = NULL; /* free the slot */ 
-        return 0; 
+/* Try to find admin in list and empties the slot */
+int adm_active_del(aNick *nick)
+{
+  int i = 0;
+
+  while(i < adminmax && adminlist[i] != nick) ++i; /* searching in list.. */
+  if(i < adminmax) adminlist[i] = NULL; /* free the slot */
+  return 0;
 }
